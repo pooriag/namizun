4d2cf215d130b37a77d12d4b4e4bd7389c5e0c2f
SiNaEs21 <46631876+SiNaEs21@users.noreply.github.com>
2024-09-12 12:08:57 +0330

    Update setup.py

--- namizun_core/setup.py
+++ namizun_core/setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(name='namizun_core',
-      version='1.3.8',
+      version='1.4.0',
       description='namizun main functions',
-      author='MalKeMit',
-      author_email='khodemalkemit@gmail.com',
-      url='https://github.com/malkemit/namizun',
+      author='SinaEs',
+      author_email='sina.eskandari0937@gmail.com',
+      url='https://github.com/sinaes21/namizun',
       setup_requires=['wheel'],
       install_requires=['psutil==5.9.4',
                         'redis==4.3.5',
