818cb8c1e8640e04be446cb7441027abbd3c2ca6
SiNaEs21 <46631876+SiNaEs21@users.noreply.github.com>
2024-09-12 12:18:26 +0330

    Update setup.py

--- namizun_menu/setup.py
+++ namizun_menu/setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='namizun_menu',
-      version='1.0.0',
+      version='1.1.0',
       description='namizun menu',
-      author='MalKeMit',
-      author_email='khodemalkemit@gmail.com',
-      url='https://github.com/malkemit/namizun',
+      author='SinaEs',
+      author_email='sina.eskandari0937@gmail.com',
+      py_modules=['main_menu', 'network_submenu', 'monitor', 'udp_submenu', 'display'],
       setup_requires=['wheel'],
       install_requires=['colored~=1.4.4',
                         'pyfiglet~=0.8.post1',
